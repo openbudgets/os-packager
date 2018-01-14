d2444c867a8aecf681e318f84df988842c663e29
liyakun <liyakun127@gmail.com>
2018-01-14 15:16:09 +0100

    upload data without filling description form

--- app/services/package.js
+++ app/services/package.js
@@ -97,57 +97,7 @@ function validateDataPackage(descriptor, schema) {
 }
 
 function createFiscalDataPackage(attributes, resources) {
-  // Use OSTypes to generate FDP
-  // TODO: Add support for more than one resource once OSTypes supports it
-  var fields = resources[0].fields;
-  _.forEach(fields, function(field) {
-    delete field.errors;
-    delete field.additionalOptions;
-    delete field.slug;
-  });
-  var fdp = new OSTypes().fieldsToModel(fields);
-
-  // Package metadata
-  _.extend(fdp, utils.removeEmptyAttributes(attributes));
-
-  // Resources
-  fdp.resources = _.map(resources, function(resource) {
-    var result = {};
-    result.name = resource.name;
-    result.format = 'csv';
-    if (resource.source.url) {
-      result.url = resource.source.url;
-    } else {
-      result.path = resource.source.fileName || resource.name + '.csv';
-    }
-    if (resource.source.mimeType) {
-      result.mediatype = resource.source.mimeType;
-    }
-    if (resource.source.size) {
-      result.bytes = resource.source.size;
-    }
-    if (resource.dialect) {
-      result.dialect = _.clone(resource.dialect);
-    }
-    if (resource.encoding) {
-      result.encoding = resource.encoding;
-    }
-    result.schema = fdp.schema;
-    result.schema.fields = _.map(
-      _.values(result.schema.fields),
-      function(field) {
-        return _.omit(field, 'options');
-      }
-    );
-    delete fdp.schema;
-    return result;
-  });
-
-  // JSON-LD
-  fdp['@context'] = 'http://schemas.frictionlessdata.io/' +
-    'fiscal-data-package.jsonld';
-
-  return fdp;
+  return require('../descriptions/fiscalDataPackage.json');
 }
 
 function convertResource(resource, dataPackage, dataPackageUrl) {
